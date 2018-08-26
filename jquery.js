7699f7a763b2eaf0c027b7e1c6a4f24e605fc9d0
Subin Sudhakaran <subinsamrat96@gmail.com>
2018-08-26 20:56:34 +0530

    First Commit

---
+++ jquery.js
@@ -0,0 +1,11 @@
+// Background while scroll
+
+$(window).on("scroll", function() {
+  if($(window).scrollTop()) {
+    $('nav').addClass('black');
+  }
+
+  else {
+    $('nav').removeClass('black');
+  }
+})
